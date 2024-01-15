8e2f5129c931dbb7fa316b310aa0f6ddf6113256
shbox <45543346+shanepaton@users.noreply.github.com>
2024-01-14 21:53:25 -0500

    Update archbox-b-chroot.sh

--- archbox-b-chroot.sh
+++ archbox-b-chroot.sh
@@ -1,56 +1,48 @@
 #!/usr/bin/env bash
-
-#                    __    __              
+#--------------------__----__--------------              
 #   ____ ___________/ /_  / /_  ____  _  __
 #  / __ `/ ___/ ___/ __ \/ __ \/ __ \| |/_/
 # / /_/ / /  / /__/ / / / /_/ / /_/ />  <  
-# \__,_/_/   \___/_/ /_/_.___/\____/_/|_|                                   
-
-# Install script by me to install Arch Linux on my laptop.
+# \__,_/_/   \___/_/ /_/_.___/\____/_/|_|
+#-----------------by shbox-----------------
 
 echo "[archbox]: Enter system hostname:"
 read HOSTNAME
-
+echo "[archbox]: Enter root password:"
+read UACRPW
 echo "[archbox]: Enter username:"
 read UACNAME
-
-echo "[archbox]: Enter  password:"
+echo "[archbox]: Enter password:"
 read UACPW
 
 echo "[archbox]: Setting timezone..."
-
-timedatectl --no-ask-password set-timezone America/Regina
-timedatectl --no-ask-password set-ntp 1
-
+ln -sf /usr/share/zoneinfo/America/Regina /etc/localtime
 hwclock --systohc
 
 echo "[archbox]: Setting Locales..."
-
 locale-gen
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 
-localectl --no-ask-password set-keymap us
-
 echo "[archbox]: Setting Hostname..."
-
-hostnamectl --no-ask-password set-hostname $HOSTNAME
+echo $HOSTNAME > /etc/hostname 
 
 echo "[archbox]: Installing packages..."
-
 pacman -S --noconfirm --needed grub efibootmgr dhcpcd iwd nano vi vim sudo neofetch cmatrix htop ntp zsh git os-prober
 
-echo "[archbox]: Creating user..."
-
-usermod -m -aG wheel $UACNAME
-
+echo "[archbox]: Creating main user..."
+useradd -m -a wheel $UACNAME
 yes $UACPW | passwd $UACNAME
 
-echo "[archbox]: Setting up GRUB..."
-
+echo "[archbox]: Configuring up GRUB..."
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 os-prober
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "[archbox]: If all went well, Arch Linux is installed."
+echo "[archbox]: Setting root password..."
+yes $UACRPW | passwd
+
+echo "[archbox]:  On restart enable iwd.service and dhcpcd.service!"
+
+echo "[archbox]: Arch Linux install complete."
 echo "[archbox]: See ya on the flip side..."
